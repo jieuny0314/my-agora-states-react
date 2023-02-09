482df66eeb8663fd9a66ae856674297923b7fa23
jieuny0314 <ccomo0313@naver.com>
2023-02-09 10:23:37 +0900

    아고라스테이츠 리액트로 리팩토링

---
+++ src/components/Tab.js
@@ -0,0 +1,14 @@
+import tab from '../images/tab.png';
+import '../styles/Tab.css';
+
+function Tab() {
+
+  return (
+    <div className="Tab">
+      <img className="tab" src={tab} alt="tab" />
+
+    </div>
+  );
+}
+
+export default Tab;
